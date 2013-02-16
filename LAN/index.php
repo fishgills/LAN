5b84da2ee39306fc62399ed79520416bf4ca7d6e
fishgills <>
2013-02-15 17:44:13 -0800

    Commit

--- LAN/index.php
+++ LAN/index.php
@@ -3,11 +3,11 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     </head>
     <body>
         asdf
         <?php
-        
         // put your code here
         ?>
     </body>
